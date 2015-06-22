4cbe4ab9ded175eba2430c1fa6e27ec302ca6b62
Gavin Thompson <gavinathompson@gmail.com>
2015-06-22 10:30:16 -0400

    adding simple gradient background to view controller for basic use case

--- gradientClass/ViewController.swift
+++ gradientClass/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
 
         // simple project to store reusable basic gradient class
+        let background = Gradient()
+        background.addAppBackgroundGradient(self.view.bounds, currentView:view )
     }
 
     override func didReceiveMemoryWarning() {
